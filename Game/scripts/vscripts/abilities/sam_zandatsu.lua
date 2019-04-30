b7379b18fb235a6c0bd48b7869c790da11ef6408
Hocok of Chaos <37977839+HocokOfChaos@users.noreply.github.com>
2019-04-30 14:15:07 +0600

    Фикс косяка с хилом сэма

--- Game/scripts/vscripts/abilities/sam_zandatsu.lua
+++ Game/scripts/vscripts/abilities/sam_zandatsu.lua
@@ -14,12 +14,12 @@ function modifier_sam_zandatsu:IsPurgable() return false end
 function modifier_sam_zandatsu:OnDeath(params)
   if params.attacker == self:GetParent() and params.attacker:IsRealHero() and params.attacker:PassivesDisabled() == false and params.unit ~= params.attacker and params.unit:IsBuilding() == false then
     if params.unit:IsRealHero() then
-      self:GetParent():Heal(self:GetParent():GetMaxHealth() * self:GetAbility():GetSpecialValueFor("hero_heal"), self:GetParent())
-      self:GetParent():GiveMana(self:GetParent():GetMaxMana() * self:GetAbility():GetSpecialValueFor("hero_heal"))
+      self:GetParent():Heal(self:GetParent():GetMaxHealth() * self:GetAbility():GetSpecialValueFor("hero_heal") / 100, self:GetParent())
+      self:GetParent():GiveMana(self:GetParent():GetMaxMana() * self:GetAbility():GetSpecialValueFor("hero_heal") / 100)
 
     else
-      self:GetParent():Heal(self:GetParent():GetMaxHealth() * self:GetAbility():GetSpecialValueFor("creep_heal"), self:GetParent())
-      self:GetParent():GiveMana(self:GetParent():GetMaxMana() * self:GetAbility():GetSpecialValueFor("creep_heal"))
+      self:GetParent():Heal(self:GetParent():GetMaxHealth() * self:GetAbility():GetSpecialValueFor("creep_heal") / 100, self:GetParent())
+      self:GetParent():GiveMana(self:GetParent():GetMaxMana() * self:GetAbility():GetSpecialValueFor("creep_heal") / 100)
 
     end
     ParticleManager:SetParticleControl(ParticleManager:CreateParticle("particles/sam/sam_zandatsu.vpcf", PATTACH_ABSORIGIN_FOLLOW, params.attacker), 0, params.attacker:GetAbsOrigin())
