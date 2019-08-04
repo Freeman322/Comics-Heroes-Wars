e30b57472fe84426392010705fe00be7bac720f4
Hocok of Chaos <37977839+HocokOfChaos@users.noreply.github.com>
2019-08-04 22:00:14 +0600

    Скиллы Чужого, фикс абилок скарлет, позже добавлю описания для абилок Чужого
    В абилках Чужого возможно отсутствует IsServer, так как я обычно туплю

---
+++ Game/scripts/vscripts/abilities/alien_bite.lua
@@ -0,0 +1,47 @@
+LinkLuaModifier("modifier_alien_bite", "abilities/alien_bite.lua", 0)
+LinkLuaModifier("modifier_alien_bite_debuff", "abilities/alien_bite.lua", 0)
+
+alien_bite = class({GetIntrinsicModifierName = function() return "modifier_alien_bite" end})
+modifier_alien_bite = class({
+    IsHidden = function() return true end,
+    IsPurgable = function() return false end,
+    DeclareFunctions = function() return {MODIFIER_EVENT_ON_ATTACK_LANDED, MODIFIER_PROPERTY_PREATTACK_CRITICALSTRIKE, MODIFIER_EVENT_ON_ORDER} end
+})
+
+function modifier_alien_bite:OnCreated() self.bite = false end
+
+function modifier_alien_bite:OnAttackLanded(params)
+    if not IsServer() then return end
+    if (self:GetAbility():GetAutoCastState() or self.bite) and self:GetAbility():IsCooldownReady() and params.attacker == self:GetParent() and self:GetParent():IsRealHero() and (params.target:IsBuilding() and params.target:IsMagicImmune() and params.target:IsOther()) == false then
+        EmitSoundOn("", self:GetCaster())
+
+        self:GetCaster():Heal(params.damage, self:GetParent())
+        params.target:AddNewModifier(self:GetParent(), self:GetAbility(), "modifier_alien_bite_debuff", {duration = self:GetAbility():GetSpecialValueFor("duration")})
+        self.bite = false
+        self:GetAbility():StartCooldown(self:GetAbility():GetCooldown(self:GetAbility():GetLevel() - 1))
+    end
+end
+
+function modifier_alien_bite:GetModifierPreAttack_CriticalStrike()
+    if (self:GetAbility():GetAutoCastState() or self.bite) and self:GetAbility():IsCooldownReady() then
+        return self:GetAbility():GetSpecialValueFor("crit_damage") + (IsHasTalent(self:GetCaster():GetPlayerOwnerID(), "special_bonus_unique_alien_bite_crit_damage") or 0)
+    end
+end
+
+function modifier_alien_bite:OnOrder(keys)
+    if keys.unit == self:GetParent() then if keys.order_type == DOTA_UNIT_ORDER_CAST_TARGET and keys.ability:GetName() == self:GetAbility():GetName() then
+        self.bite = true
+    else
+        self.bite = false
+    end
+    end
+end
+
+modifier_alien_bite_debuff = class({
+    IsHidden = function() return false end,
+    IsPurgable = function() return true end,
+    DeclareFunctions = function() return {MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE, MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT} end
+})
+
+function modifier_alien_bite_debuff:GetModifierMoveSpeedBonus_Percentage() return self:GetAbility():GetSpecialValueFor("ms_slow_pct") * -1 end
+function modifier_alien_bite_debuff:GetModifierAttackSpeedBonus_Constant() return self:GetAbility():GetSpecialValueFor("as_slow") * -1 end
---
+++ Game/scripts/vscripts/abilities/alien_evolution.lua
@@ -0,0 +1,21 @@
+LinkLuaModifier("modifier_alien_evolution", "abilities/alien_evolution.lua", 0)
+
+alien_evolution = class({GetIntrinsicModifierName = function() return "modifier_alien_evolution" end})
+
+function alien_evolution:GetCooldown(nLevel) return self.BaseClass.GetCooldown(self, nLevel) - (IsHasTalent(self:GetCaster():GetPlayerOwnerID(), "special_bonus_unique_alien_evolution_cd_red") or 0) end
+modifier_alien_evolution = class({IsHidden = function() return true end, IsPurgable = function() return false end, GetAttributes = function() return MODIFIER_ATTRIBUTE_PERMANENT end})
+
+function modifier_alien_evolution:OnCreated() self.cd = 0 if not IsServer() then return end self:StartIntervalThink(0.03) end
+function modifier_alien_evolution:OnIntervalThink()
+    if not IsServer() then return end
+    if self:GetParent():IsRealHero() and self:GetAbility():IsCooldownReady() then
+        self:GetCaster():ModifyStrength(self:GetAbility():GetSpecialValueFor("bonus_stats"))
+        self:GetCaster():ModifyAgility(self:GetAbility():GetSpecialValueFor("bonus_stats"))
+        self:GetCaster():ModifyIntellect(self:GetAbility():GetSpecialValueFor("bonus_stats"))
+
+        EmitSoundOn("", self:GetCaster())
+
+        self:GetAbility():EndCooldown()
+        self:GetAbility():StartCooldown(self:GetAbility():GetCooldown(self:GetAbility():GetLevel() - 1))
+    end
+end
