0f4f39cbef61cd5fa332703d9349b98f9b4dd62e
Adam Smith <adamsmith1852@gmail.com>
2019-09-16 19:48:58 +0300

    ica pearl cannot crash game

--- Game/scripts/vscripts/items/item_ice_pearl.lua
+++ Game/scripts/vscripts/items/item_ice_pearl.lua
@@ -44,28 +44,24 @@ function modifier_item_ice_pearl:GetModifierBonusStats_Strength() return self:Ge
 function modifier_item_ice_pearl:GetModifierHealthBonus() return self:GetAbility():GetSpecialValueFor("bonus_health") end
 
 function modifier_item_ice_pearl:OnTakeDamage(params)
-  if IsServer() then
-    if params.unit == self:GetParent() and self:GetParent():HasModifier("modifier_item_ice_pearl_cooldown") == false and params.attacker ~= self:GetParent() and params.attacker:IsBuilding() == false and params.attacker:IsRealHero() and self:GetParent():IsRealHero() then
+    if IsServer() then
+        if params.unit == self:GetParent() and self:GetParent():HasModifier("modifier_item_ice_pearl_cooldown") == false and params.attacker ~= self:GetParent() and params.attacker:IsBuilding() == false and params.attacker:IsRealHero() and self:GetParent():IsRealHero() then
+            params.attacker:AddNewModifier(self:GetParent(), self:GetAbility(), "modifier_item_ice_pearl_reduction", {duration = self:GetAbility():GetSpecialValueFor("debuff_duration")})
+            self:GetParent():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_item_ice_pearl_cooldown", {duration = self:GetAbility():GetSpecialValueFor("pearl_shield_cooldown")})
+            
+            EmitSoundOn("Hero_Winter_Wyvern.WintersCurse.Cast", params.attacker)
+            EmitSoundOn("Hero_Winter_Wyvern.ColdEmbrace.Cast", params.attacker)
 
-      params.attacker:AddNewModifier(self:GetParent(), self:GetAbility(), "modifier_item_ice_pearl_reduction", {duration = self:GetAbility():GetSpecialValueFor("debuff_duration")})
-      
-      EmitSoundOn("Hero_Winter_Wyvern.WintersCurse.Cast", params.attacker)
-      EmitSoundOn("Hero_Winter_Wyvern.ColdEmbrace.Cast", params.attacker)
-
-      ApplyDamage({
-          victim = params.attacker,
-          attacker = self:GetParent(),
-          damage = params.original_damage * self:GetAbility():GetSpecialValueFor("damage_return") / 100,
-          damage_type = DAMAGE_TYPE_PURE,
-          ability = self:GetAbility(),
-          damage_flags = DOTA_DAMAGE_FLAG_REFLECTION + DOTA_DAMAGE_FLAG_NO_SPELL_AMPLIFICATION
-      })
-
-        ---self:GetParent():Heal(params.damage, self:GetAbility())
-
-      self:GetParent():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_item_ice_pearl_cooldown", {duration = self:GetAbility():GetSpecialValueFor("pearl_shield_cooldown")})
+            ApplyDamage({
+                victim = params.attacker,
+                attacker = self:GetParent(),
+                damage = params.original_damage * self:GetAbility():GetSpecialValueFor("damage_return") / 100,
+                damage_type = DAMAGE_TYPE_PURE,
+                ability = self:GetAbility(),
+                damage_flags = DOTA_DAMAGE_FLAG_REFLECTION + DOTA_DAMAGE_FLAG_NO_SPELL_AMPLIFICATION
+            })
+        end
     end
-  end
 end
 
 modifier_item_ice_pearl_reduction = class({})
@@ -91,7 +87,8 @@ function modifier_item_ice_pearl_active:DeclareFunctions() return {MODIFIER_EVEN
 function modifier_item_ice_pearl_active:OnTakeDamage(params)
     if IsServer() then
         if params.unit == self:GetParent() and params.attacker ~= self:GetParent() and params.attacker:GetClassname() ~= "ent_dota_fountain" and params.attacker:HasModifier("modifier_item_ice_pearl_active") == false then
-
+            EmitSoundOn("DOTA_Item.BladeMail.Damage", params.attacker)
+            
             ApplyDamage ({
                 victim = params.attacker,
                 attacker = self:GetParent(),
@@ -100,8 +97,6 @@ function modifier_item_ice_pearl_active:OnTakeDamage(params)
                 ability = self:GetAbility(),
                 damage_flags = DOTA_DAMAGE_FLAG_REFLECTION + DOTA_DAMAGE_FLAG_NO_SPELL_AMPLIFICATION
             })
-
-            EmitSoundOn("DOTA_Item.BladeMail.Damage", params.attacker)
         end
     end
 end
