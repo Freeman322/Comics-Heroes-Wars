86dc711d02a3c55b0d7b8569d6c23c19b9c2d6a7
Adam Smith <adamsmith1852@gmail.com>
2019-12-12 22:28:24 +0300

    fixes

--- Content/panorama/scripts/Lib.js
+++ Content/panorama/scripts/Lib.js
@@ -5373,6 +5373,20 @@ Lib.Items = {
         "is_medal": "0",
         "is_compendium": "0",
         "is_music": "0"
+    },
+    "395": {
+        "item": "iron_devil",
+        "rarity": "11",
+        "quality": "2",
+        "dropable": "false",
+        "slot": "global",
+        "hero": "npc_dota_hero_juggernaut",
+        "universal": "false",
+        "def_id": "395",
+        "is_treasure": "0",
+        "is_medal": "0",
+        "is_compendium": "0",
+        "is_music": "0"
     }
  };
 
